8004d609bcb953c94aaec21d7ed745116ce9f32d
cotetus <brucelydemello@gmail.com>
2018-09-25 04:58:08 -0300

    Frontend edited, menu Adm

--- view/admin/esp/listEsp.php
+++ view/admin/esp/listEsp.php
@@ -5,8 +5,10 @@
         	<img id="esp-img" <?php echo "src = 'assets/image/".$item['img']."'";?>/>
         	<h3><?php echo $item['nombre'];?></h3>
         	<p><?php echo $item['artista'];?></p>
+          <a href="#">Ver espectáculo</a>
+          <a href="?c=esp&a=Index">Editar</a>
            </div>
   		<?php endforeach; ?>
        </div>
-       </div>
+
     
